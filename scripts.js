a5c5860e831a447bb2b77145774ea4311c34c6ab
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-03-15 11:28:09 +0300

    Add hover functions in js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
+$(".dgn").click(function () {
+  $(".dgn").hide();
+  $(".prd1").show();
+});
 
+$(".prd1").click(function () {
+  $(".prd1").hide();
+  $(".dgn").show();
+});
 
+$(".dvt").click(function () {
+  $(".dvt").hide();
+  $(".prd2").show();
+});
 
+$(".prd2").click(function () {
+  $(".prd2").hide();
+  $(".dvt").show();
+});
+
+$(".mgt").click(function () {
+  $(".mgt").hide();
+  $(".prd3").show();
+});
+
+$(".prd3").click(function () {
+  $(".prd3").hide();
+  $(".mgt").show();
+});
 
 var studio = [0, 1, 2, 3, 4, 5, 6, 7];
 studio.forEach(function (ele, index) {
